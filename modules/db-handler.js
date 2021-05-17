c7adbe79ecc601c270344649cc3401f3b217482b
SciDev5 <jhuber2048@gmail.com>
2021-05-17 15:56:37 -0400

    k

--- modules/db-handler.js
+++ modules/db-handler.js
@@ -6,7 +6,7 @@ import { APPROVAL_STATUSES, PRIVACY_STATUSES, PLATFORMS, SUBJECTS, GRADE_LEVELS,
 import pg from "pg";
 import { v4 as genUUID } from "uuid";
 import fs from "fs";
-const client = new pg.Pool();
+const client = new pg.Pool({connectionString:process.env.DATABASE_URL});
 
 var allAppsCache = undefined;
 
